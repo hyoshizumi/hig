8bfb077d6a459b7fd62e6608732c92f0b58f5d8b
Sean Durham <sean@sdurham.net>
2017-09-08 22:43:11 -0700

    Add remaining interface to Button

--- src/implementations/react/src/adapters/NewButtonAdapter.js
+++ src/implementations/react/src/adapters/NewButtonAdapter.js
@@ -5,13 +5,20 @@ import HIGAdapter, { Prop, EventHandler } from './HIGAdapter';
 function NewButtonAdapter(props) {
   return (
     <HIGAdapter name="Button" HIGConstructor={HIG.Button}>
-      <Prop value={props.type} setter="setType" />
+      <Prop value={props.icon} setter="setIcon" />
+      <Prop value={props.link} setter="setLink" />
+      <Prop value={props.target} setter="setTarget" />
       <Prop value={props.title} setter="setTitle" />
+      <Prop value={props.type} setter="setType" />
+      <Prop value={props.size} setter="setSize" />
       <Prop value={props.width} setter="setWidth" />
       <Prop value={props.disabled}>
         {(instance, value) => value ? instance.disable() : instance.enable() }
       </Prop>
+      <EventHandler value={props.onBlur} setter="onBlur" />
       <EventHandler value={props.onClick} setter="onClick" />
+      <EventHandler value={props.onFocus} setter="onFocus" />
+      <EventHandler value={props.onHover} setter="onHover" />
     </HIGAdapter>
   );
 }
